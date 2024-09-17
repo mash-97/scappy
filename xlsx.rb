88c79c81fbefa6d31077619ac518c41c26c20d7a
mash-97 <mahimnhd97@gmail.com>
2024-09-17 17:50:23 +0600

    remove debug code

--- xlsx.rb
+++ xlsx.rb
@@ -13,21 +13,16 @@ class Xlsx
     end
   end
   def write()
-    puts("i> write current state")
     @ap.serialize(@file_name)
   end
 
   private
   def loadXlsx()
     if not File.exists?(@file_name) then
-      puts("i> file not found")
       @ap.serialize(@file_name)
-      puts("i> file created: #{@file_name}")
     end
-    puts("i> load with rubyXL")
     begin 
       rxl_wrkbook = RubyXL::Parser.parse(@file_name)
-      puts("i> total worksheets to load: #{rxl_wrkbook.worksheets.size}")
       rxl_wrkbook.worksheets.each do |rx_ws|
         @ap.workbook.add_worksheet(name: rx_ws.sheet_name) do |ap_sheet|
           rx_ws.each_with_index do |rx_row, rx_indx|
@@ -35,8 +30,6 @@ class Xlsx
           end
         end
       end
-      puts("i> load complete")
-      puts("i> total loaded worksheets: #{@ap.workbook.worksheets.size}")
     rescue => e
       puts("#> load error [rubyXL]: #{e}")
     end
