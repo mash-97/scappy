9805932199b72da1e23910ef20f38e0c93a5d30a
mash-97 <mahimnhd97@gmail.com>
2024-09-18 00:22:49 +0600

    rest-client header

--- articles.rb
+++ articles.rb
@@ -12,7 +12,9 @@ class Articles
     
     private
     def get_nokogorized_page(url)
-      response = $CLIENT.get(url)
+      headers = {}
+      headers[:user_agent] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+      response = $CLIENT.get(url, headers: headers)
       if response.code==200 then
         return Nokogiri::HTML(response.body)
       end
