6d422bc4dc2ad22a49d0b7f5580baca06b5551ba
mash-97 <mahimnhd97@gmail.com>
2024-09-20 14:02:04 +0600

    rotate headers

--- batch-req.rb
+++ batch-req.rb
@@ -1,8 +1,20 @@
 require 'async'
 require 'rest-client'
 
-$__headers__ = {}
-$__headers__[:user_agent] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+
+
+def get_headers()
+  user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
+    "curl/7.68.0"
+  ]
+  headers = {}
+  headers["User-Agent"] = user_agents.sample
+  return headers
+end
 
 class BatchReq
   def initialize(urls, batch_size=10, max_timeout=10, max_attempts=3)
@@ -65,7 +77,7 @@ class BatchReq
   def get(url, timeout, &callback)
     response = nil
     begin
-      response = RestClient.get(url, headers: $__headers__, timeout: timeout)
+      response = RestClient.get(url, headers: get_headers(), timeout: timeout)
     rescue => e  
       puts("#>> RestClient Error: #{e}")
       response = nil
